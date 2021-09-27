f83c08f1a40a43f50e668a25a9ede61eac2a7c91
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2021-09-28 08:13:17 +0900

    水の入った大釜に投げ入れた時の反応を正常に動作するよう修正

--- src/main/java/pro/freeserver/alphakun/plugin/chemistry/events/WaterReaction.kt
+++ src/main/java/pro/freeserver/alphakun/plugin/chemistry/events/WaterReaction.kt
@@ -11,36 +11,30 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.inventory.ItemStack
 import pro.freeserver.alphakun.plugin.chemistry.substances.Sodium
+import kotlin.reflect.typeOf
 
 class WaterReaction : Listener {
 
     @EventHandler
     fun entityInsideBlock(e: EntityInsideBlockEvent) {
         if (e.entityType == EntityType.DROPPED_ITEM) {
-            TODO("この下が動作しないので要変更")
             val item = e.entity as Item
             val loc = item.location
-            if (e.block.type == Material.WATER) {
-                if (itemDetection(item.itemStack, loc) != null){
-                    item.remove()
-                    loc.world.dropItem(loc,itemDetection(item.itemStack, loc)!!)
-                }
-            } else if (e.block.type == Material.CAULDRON && e.block is Levelled) {
-                val cauldronData = e.block as Levelled
+            if (e.block.type == Material.CAULDRON) {
+                val cauldronData = e.block.blockData as Levelled
                 if (cauldronData.level > 0) {
-                    if (itemDetection(item.itemStack, loc) != null){
-                        item.remove()
-                        loc.world.dropItem(loc,itemDetection(item.itemStack, loc)!!)
-                    }
+                    item.itemStack = itemDetection(item.itemStack, loc)
+                    cauldronData.level-=1
+                    e.block.blockData = cauldronData
                 }
             }
         }
     }
 
-    fun itemDetection(item: ItemStack, loc: Location): ItemStack? {
+    fun itemDetection(item: ItemStack, loc: Location): ItemStack {
         if (Sodium.isSubstance(item)) {
             return Sodium.waterReaction(item,loc)
         }
-        return null
+        return item
     }
 }
